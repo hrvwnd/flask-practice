dae52b21ce66a9325fc4737ce5df8052221a5ca0
hrvwnd <harveyawendon@gmail.com>
2019-12-02 18:43:37 +0000

    Added hyperlink in layout

--- application/routes.py
+++ application/routes.py
@@ -54,7 +54,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             if next_page:
