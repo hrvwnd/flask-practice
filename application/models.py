b7630942f5547ebeff13454591c710feaa512ce2
hrvwnd <harveyawendon@gmail.com>
2019-12-03 09:54:06 +0000

    Added 'checking our classes'

--- application/models.py
+++ application/models.py
@@ -2,20 +2,6 @@ from application import db,login_manager
 from flask_login import UserMixin
 from datetime import datetime
 
-
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False, unique=True)
-    content = db.Column(db.String(100), nullable=False, unique=True)
-    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
-    def __repr__(self):
-        return ''.join([
-            'User_ID: ', self.first_name, ' ', self.last_name, '\r\n',
-            'Title: ', self.title, '\r\n', self.content
-        ])
-
-
 class Users(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), nullable=False)
@@ -30,6 +16,20 @@ class Users(db.Model,UserMixin):
             'Email: ', self.email,
             'Name', self.first_name,' ', self.last_name
             ])
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    content = db.Column(db.String(100), nullable=False, unique=True)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    def __repr__(self):
+        return ''.join([
+            'User_ID: ', self.first_name, ' ', self.last_name, '\r\n',
+            'Title: ', self.title, '\r\n', self.content
+        ])
+
 
 @login_manager.user_loader
 def load_user(id):
